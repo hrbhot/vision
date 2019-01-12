7ef9cff82ee89e6205ae9117a33b5557c83087cf
hrbhot <hrbhot@gmail.com>
2019-01-12 22:51:33 +0100

    fix build

--- ops/sh/build.sh
+++ ops/sh/build.sh
@@ -4,7 +4,7 @@ VER=0.0.2
 if [ ! "$(docker ps -q -f name=flask-ut)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=flask-ut)" ]; then
         # cleanup
-        docker rm flask
+        docker rm flask-ut
     fi
     # run your container
     docker run -d --name=flask-ut -p 5000:5000 hrbhot/flask:$VER bash -c 'tail -f /dev/null'
