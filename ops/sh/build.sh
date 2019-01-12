c5546ee5f9a1403ebc16b53d11d40e9914086f1b
hrbhot <hrbhot@gmail.com>
2019-01-12 14:22:58 +0100

    separate container name

--- ops/sh/build.sh
+++ ops/sh/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 VER=0.0.2
-if [ ! "$(docker ps -q -f name=flask)" ]; then
-    if [ "$(docker ps -aq -f status=exited -f name=flask)" ]; then
+if [ ! "$(docker ps -q -f name=flask-ut)" ]; then
+    if [ "$(docker ps -aq -f status=exited -f name=flask-ut)" ]; then
         # cleanup
         docker rm flask
     fi
     # run your container
-    docker run -d --name=flask -p 5000:5000 hrbhot/flask:$VER bash -c 'tail -f /dev/null'
+    docker run -d --name=flask-ut -p 5000:5000 hrbhot/flask:$VER bash -c 'tail -f /dev/null'
 fi
